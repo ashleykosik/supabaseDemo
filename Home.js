360b889f788f5aa5e57538f77b27e1319b227b5f
ashleykosik <127979865+ashleykosik@users.noreply.github.com>
2023-08-09 15:01:45 -0400

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,9 +1,37 @@
 import supabase from '/supabase'
+import {useEffect, useState } from 'react'
+
 
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+  
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+      .from('smoothies')
+      .select()
+
+      if(error) {
+        setFetchError('could not fetch')
+        setSmoothies(null)
+        console.log(error)
+    }
+      if(data) {
+        setSmoothies(data)
+        setFetchError(null)
+      }
+  fetchSmoothies()
+      
+  }, [])
   return (
     <div className = "page home">
-    <h2>Home</h2>
+    {fetchError &&(<p>{fetchError}</p>)}
+    {smoothies && (
+      <div classname='smoothies'>
+      {smoothies.map(smoothie => {
+        <p>{smoothies.title}</p>
+      </div>
     </div>
   )
 }
